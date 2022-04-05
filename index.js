5a26e0c9dcff310ab452374020a80f8c9f5a152b
Daniel Passarelli <35617086+DanielP53@users.noreply.github.com>
2022-04-04 23:16:00 -0600

    Added load more functionality and search functionality

--- index.js
+++ index.js
@@ -8,8 +8,44 @@ fetch('https://api.giphy.com/v1/gifs/trending?api_key=J5CCLFL7rpPbWweAEIFM2bNpfd
         data.data.forEach(element => {
             let insertElement = document.createElement('img');
 
-            insertElement.className = 'col-3 m-2 img-fluid'
+            insertElement.className = 'col-3 m-2 rounded img-fluid'
             insertElement.src = element.images.original.url;
             container.appendChild(insertElement);
         });
   });
+
+function getMore() {
+    const container = document.querySelector('.gif-container');
+
+    for (let i = 0; i < 10; i++) {
+        fetch('https://api.giphy.com/v1/gifs/random?api_key=J5CCLFL7rpPbWweAEIFM2bNpfdonszs4&tag=&rating=g')
+            .then(response => response.json())
+            .then(data => {
+                let insertElement = document.createElement('img');
+
+                insertElement.className = 'col-3 m-2 rounded img-fluid'
+                insertElement.src = data.data.images.original.url;
+                container.appendChild(insertElement);
+            });
+    }
+}
+
+function search() {
+    const container = document.querySelector('.gif-container');
+
+    container.innerHTML = '';
+
+    const query = document.querySelector('.query').value;
+
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=J5CCLFL7rpPbWweAEIFM2bNpfdonszs4&q=${query}&limit=10&offset=0&rating=g&lang=en`)
+        .then(response => response.json())
+        .then(data => {
+            data.data.forEach(element => {
+                let insertElement = document.createElement('img');
+    
+                insertElement.className = 'col-3 m-2 rounded img-fluid'
+                insertElement.src = element.images.original.url;
+                container.appendChild(insertElement);
+            }); 
+        });
+}
